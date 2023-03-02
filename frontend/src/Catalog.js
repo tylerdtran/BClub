e4c990dc78dea879fdf56ce7da12e1579db75451
Max Deng <deng.m.842@gmail.com>
2023-03-01 19:26:03 -0800

    Added a club catalog page
    Added a club catalog page. Compatibility with other modules and
    dark mode is maintained. Added a directory for club catalog images.

--- frontend/src/Catalog.js
+++ frontend/src/Catalog.js
@@ -1,10 +1,431 @@
 import './App.css';
+import './Catalog.css';
 import React from 'react';
 
 export default function Catalog(){
 	return(
 		<>
-			<p className="errorMessage">Catalog: Under construction, come back later!</p>
+			<div className="catalogMain">
+				<h2>Featured</h2>
+				<section className="category">
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Genshin Impact at UCLA
+							</a>
+							<br/>
+							<br/>
+							The definitive community for Genshin Impact at UCLA!
+						</p>
+						<img className="clubImage"
+							src="clubcatalogimages/genshin_club.webp"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+				</section>
+
+				<h2>Community</h2>
+				<section className="category">
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>			
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>			
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+				</section>
+
+				<h2>Educational</h2>
+				<section className="category">
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+				</section>
+
+				<h2>Resume Builders</h2>
+				<section className="category">
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+				</section>
+
+				<h2>Social Activism</h2>
+				<section className="category">
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+				</section>
+
+				<h2>Outreach</h2>
+				<section className="category">
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+				</section>
+
+				<h2>Other</h2>
+				<section className="category">
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+					<div className="clubListing">
+						<p className="clubBlurb">
+							<a href="#">
+								Coming soon!
+							</a>
+							<br/>
+							<br/>
+							Coming soon!
+						</p>
+						<img className="clubImage"
+							src="placeholder.png"
+							alt="Club Listing"
+						/>
+					</div>
+				</section>
+			</div>
 		</>
 	);
 }
