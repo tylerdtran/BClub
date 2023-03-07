ec3e4c15d98b78034a00cbd5d5ba9bbe6de0fdb1
William Park <williampark@Williams-MacBook-Air.local>
2023-03-07 12:34:34 -0800

    Modified Highlights page to display posts
    Highlights.js
     - removed useState from imports (unnecessary)
     - created Post function to modularize posts (passing in link and social media)
     - hard-coded example posts
    
    Highlights.ccs
     - posts are horizontally scrollable

--- frontend/src/Highlights.js
+++ frontend/src/Highlights.js
@@ -1,11 +1,104 @@
-// import React, { useState } from "react";
-// import { FacebookEmbed } from 'react-social-media-embed';
-// import { InstagramEmbed } from 'react-social-media-embed';
-// import { LinkedInEmbed } from 'react-social-media-embed';
-// import { PinterestEmbed } from 'react-social-media-embed';
-// import { TikTokEmbed } from 'react-social-media-embed';
-// import { TwitterEmbed } from 'react-social-media-embed';
-// import { YouTubeEmbed } from 'react-social-media-embed';
+import React from "react";
+import { FacebookEmbed } from 'react-social-media-embed';
+import { InstagramEmbed } from 'react-social-media-embed';
+import { LinkedInEmbed } from 'react-social-media-embed';
+import { PinterestEmbed } from 'react-social-media-embed';
+import { TikTokEmbed } from 'react-social-media-embed';
+import { TwitterEmbed } from 'react-social-media-embed';
+import { YouTubeEmbed } from 'react-social-media-embed';
 
-// export default function Highlights() {
-// }
+function Post({ link, socialMedia }) {
+    if(socialMedia=="facebook") {
+        return(<FacebookEmbed url={link} />);
+    }
+    else if(socialMedia=="instagram") {
+        return(<InstagramEmbed url={link} />);
+    }
+    else if(socialMedia=="linkedin") {
+        return(<LinkedInEmbed url={link} />);
+    }
+    else if(socialMedia=="pinterest") {
+        return(<PinterestEmbed url={link} />);
+    }
+    else if(socialMedia=="tiktok") {
+        return(<TikTokEmbed url={link}  />);
+    }
+    else if(socialMedia=="twitter") {
+        return(<TwitterEmbed url={link} />);
+    }
+    else if(socialMedia=="youtube") {
+        return(<YouTubeEmbed url={link} />);
+    }
+}
+
+export default function Highlights() {
+    return (
+        <>
+            <h2>Facebook</h2>
+            <div className="horizontal-scroll-wrapper"> 
+                <div className="squares">
+                    <Post link="https://www.facebook.com/UCLA/posts/10160133154795958"
+                    socialMedia="facebook"/>
+                </div>
+                <div className="squares">
+                    <Post link="https://www.facebook.com/UCLA/posts/10160134035360958"
+                    socialMedia="facebook"/>
+                </div>
+                <div className="squares">
+                    <Post link="https://www.facebook.com/UCLA/posts/10160134339840958"
+                    socialMedia="facebook"/>
+                </div>
+            </div>
+            <h2>Instagram</h2>
+            <div className="horizontal-scroll-wrapper"> 
+                <div className="squares">
+                    <Post link="https://www.instagram.com/reel/CpVnexJgg2O/?utm_source=ig_web_copy_link"
+                    socialMedia="instagram"/>
+                </div>
+                <div className="squares">
+                    <Post link="https://www.instagram.com/p/CpWi_1UOTCK/?utm_source=ig_web_copy_link"
+                    socialMedia="instagram"/>
+                </div>
+                <div className="squares">
+                    <Post link="https://www.instagram.com/p/CpYYlaJtEYE/?utm_source=ig_web_copy_link"
+                    socialMedia="instagram"/>
+                </div>
+            </div>
+            <h2>LinkedIn</h2>
+            <div className="horizontal-scroll-wrapper"> 
+                <div className="squares">
+                    <Post link="https://www.linkedin.com/embed/feed/update/urn:li:share:6898694772484112384"
+                    socialMedia="linkedin"/>
+                </div>
+            </div>
+            <h2>Pinterest</h2>
+            <div className="horizontal-scroll-wrapper"> 
+                <div className="squares">
+                    <Post link="https://www.pinterest.co.uk/pin/875105771321194304/"
+                    socialMedia="pinterest"/>
+                </div>
+            </div>
+            <h2>TikTok</h2>
+            <div className="horizontal-scroll-wrapper"> 
+                <div className="squares">
+                    <Post link="https://www.tiktok.com/@epicgardening/video/7055411162212633903"
+                    socialMedia="tiktok"/>
+                </div>
+            </div>
+            <h2>Twitter</h2>
+            <div className="horizontal-scroll-wrapper"> 
+                <div className="squares">
+                    <Post link="https://twitter.com/PixelAndBracket/status/1356633038717923333"
+                    socialMedia="twitter"/>
+                </div>
+            </div>
+            <h2>YouTube</h2>
+            <div className="horizontal-scroll-wrapper"> 
+                <div className="squares">
+                    <Post link="https://www.youtube.com/watch?v=HpVOs5imUN0"
+                    socialMedia="youtube"/>
+                </div>
+            </div>
+        </>
+    );
+}
