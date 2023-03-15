ca36fe410c8b46f51054888f255751956e26fff1
David Harris <davidjoharris@gmail.com>
2023-03-14 22:54:25 -0700

    Catalog displays newest clubs created

--- frontend/src/Catalog.js
+++ frontend/src/Catalog.js
@@ -1,34 +1,58 @@
 import './App.css';
 import './Catalog.css';
-import React from 'react';
-// import {
-//     MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple
-// } from 'mdb-react-ui-kit';
+import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { db } from "./Firebase";
+import { ref, orderByChild, limitToLast, get, query, equalTo } from "firebase/database";
 
 export default function Catalog() {
+	const [newname1, setNewName1] = useState("");
+	const [newname2, setNewName2] = useState("");
+	const [newname3, setNewName3] = useState("");
+	const [newname4, setNewName4] = useState("");
+
+	useEffect(()=>{
+		const latestClubs = query(ref(db, '/clubs'), orderByChild('createdAt'), limitToLast(4));
+
+		get(latestClubs).then((snapshot) =>{
+		  if(snapshot.exists()){
+			var clubs = snapshot.val();
+			var keys = Object.keys(clubs);
+			setNewName1(clubs[keys[0]].name);
+			setNewName2(clubs[keys[1]].name);
+			setNewName3(clubs[keys[2]].name);
+      setNewName4(clubs[keys[3]].name);
+
+    } else{
+			console.log("no data");
+		  }
+		}).catch((error) => {
+		  console.error(error)
+		})
+	})
+
     const categories = [
       {
-        name: "Featured",
+        name: "New Clubs",
         clubs: [
           {
-            clubName: "Genshin Impact at UCLA",
-            clubBlurb: "The definitive community for Genshin Impact at UCLA!",
-            clubImage: "clubcatalogimages/genshin_club.webp",
+            clubName: newname1,
+            clubBlurb: "",
+            clubImage: "",
           },
           {
-            clubName: "Bruin Club Tennis",
-            clubBlurb: "The best club ever!",
-            clubImage: "clubcatalogimages/BruinClubTennis.png",
+            clubName: newname2,
+            clubBlurb: "",
+            clubImage: "",
           },
           {
-            clubName: "Coming Soon",
+            clubName: newname3,
             clubBlurb: "Coming Soon",
             clubImage: "placeholder.png",
           },
           {
-            clubName: "Coming Soon",
+            clubName: newname4,
             clubBlurb: "Coming Soon",
             clubImage: "placeholder.png",
           },
@@ -171,3 +195,148 @@ function Category({ category, clubs }) {
       </div>
     );
 }
+
+
+
+// import './App.css';
+// import './Catalog.css';
+// import React, { useState, useEffect } from 'react';
+// import {
+// 	MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple
+// } from 'mdb-react-ui-kit';
+// import { db } from "./Firebase";
+// import { ref, orderByChild, limitToLast, get, query, equalTo } from "firebase/database";
+
+// function Club({ clubName, clubBlurb, clubImage }) {
+// 	return (
+// 		<div className="clubListing">
+// 			<p className="clubBlurb">
+// 				<a href="#">
+// 					{clubName}
+// 				</a>
+// 				<br />
+// 				<br />
+// 				{clubBlurb}
+// 			</p>
+// 			<img className="clubImage"
+// 				src={clubImage}
+// 				alt="Club Listing"
+// 			/>
+// 		</div>
+// 	);
+// }
+
+// function NewClubsCategory() {
+// 	const [name, setName] = useState("");
+// 	const [description, setDescription] = useState("");
+// 	const [name2, setName2] = useState("");
+// 	const [description2, setDescription2] = useState("");
+// 	// const [name3, setName3] = useState("");
+// 	// const [description3, setDescription3] = useState("");
+// 	// const [name4, setName4] = useState("");
+// 	// const [description4, setDescription4] = useState("");
+// 	useEffect(()=>{
+// 		const sortedClubTimestamps = query(ref(db, '/clubs'), orderByChild('createdAt'));
+// 		const latestClubs = query(sortedClubTimestamps, limitToLast(2)); //need more clubs
+
+// 		get(latestClubs).then((snapshot) =>{
+// 		  if(snapshot.exists()){
+// 			var clubs = snapshot.val();
+// 			var keys = Object.keys(clubs);
+// 			// setName4(clubs[keys[0]].name);
+// 			// setDescription4(clubs[keys[0]].description);
+// 			// setName3(clubs[keys[1]].name);
+// 			// setDescription3(clubs[keys[1]].description);
+// 			setName2(clubs[keys[1]].name);
+// 			setDescription2(clubs[keys[1]].description);
+// 			setName(clubs[keys[0]].name);
+// 			setDescription(clubs[keys[0]].description);
+// 		} else{
+// 			console.log("no data");
+// 		  }
+// 		}).catch((error) => {
+// 		  console.error(error)
+// 		})
+// 	})
+
+
+
+// 	return (
+// 		<div>
+// 			<h2>{ "New Clubs" }</h2>
+// 			<section className="category">
+// 				<Club clubName={name}
+// 				clubBlurb={description}
+// 				clubImage="clubcatalogimages/genshin_club.webp"
+// 				/>
+// 				<Club clubName={name2}
+// 				clubBlurb=""
+// 				clubImage="clubcatalogimages/genshin_club.webp"
+// 				/>
+// 			</section>
+// 		</div>
+		
+		
+// 	);
+// }
+
+
+// function Category({ category, clubCategoryAttribute }) {
+// 	const [name, setName] = useState("");
+// 	const [description, setDescription] = useState("");
+
+// 	useEffect(()=>{
+// 		const categorySortedClubs = query(ref(db, '/clubs'), orderByChild('clubType'), equalTo(clubCategoryAttribute));
+
+
+// 		get(categorySortedClubs).then((snapshot) =>{
+// 			if(snapshot.exists()){
+// 				var clubs = snapshot.val();
+// 				var keys = Object.keys(clubs);
+// 				setName(clubs[keys[0]].name);
+// 		  		setDescription(clubs[keys[0]].description);
+// 	  } 	else{
+// 		  console.log("no data");
+// 		}
+// 	  }).catch((error) => {
+// 		console.error(error)
+// 	  })
+// 	})
+// 	return (
+// 		<div>
+// 			<h2>{ category }</h2>
+// 			<section className="category">
+// 				<Club clubName={name}
+// 				clubBlurb=""
+// 				clubImage="clubcatalogimages/genshin_club.webp"
+// 				/>
+// 				<Club clubName="Coming Soon"
+// 				clubBlurb="Coming Soon"
+// 				clubImage="placeholder.png"
+// 				/>
+// 				<Club clubName="Coming Soon"
+// 				clubBlurb="Coming Soon"
+// 				clubImage="placeholder.png"
+// 				/>
+// 				<Club clubName="Coming Soon"
+// 				clubBlurb="Coming Soon"
+// 				clubImage="placeholder.png"
+// 				/>
+// 			</section>
+// 		</div>
+// 	);
+// }
+
+// export default function Catalog() {
+// 	return (
+// 		<>
+// 			<div className="catalogMain">
+// 				<NewClubsCategory/>
+// 				<Category category="Academic Clubs" clubCategoryAttribute='academic'/>
+// 				{/* <Category category="Resume Builders" />
+// 				<Category category="Sports" />
+// 				<Category category="Cultural" /> */}
+// 			</div>
+// 		</>
+// 	);
+// }
