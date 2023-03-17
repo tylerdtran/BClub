0de3be1906bb58b4bce06bb5d310ad83e4e9eb9e
md842 <deng.m.842@gmail.com>
2023-03-16 19:28:47 -0700

    Cosmetic changes to add events form
    Cosmetic changes to add events form

--- frontend/src/GoogleCalendar.js
+++ frontend/src/GoogleCalendar.js
@@ -58,18 +58,21 @@ export default function GoogleCalendar() {
   }
   return (
     <div>
-      <GoogleButton onClick={GoogleLogIn}/>
+      <h2 className="eventsFormTitle">Sign in with Google to find Google Calendar events to add!</h2>
+      <GoogleButton className="eventsFormGoogleButton" onClick={GoogleLogIn}/>
       {events.map((item, index) => (
         <>
-          <p>Title: {item[0]}</p>
-          <p>Start Date: {item[1]}</p>
-          <p>End Date: {item[2]}</p>
-          <p>Description: {item[3]}</p>
-          <p>Location: {item[4]}</p>
-          <button onClick={() => addEvent(item[0], item[1], item[2], item[3], item[4], index)} 
-          disabled={clicked[index]}>
-            {clicked[index] ? "Event Added" : "Add Event"}
-          </button>
+          <div className="eventToAdd">
+            <p className="eventTitle">Title: {item[0]}</p>
+            <p>Start Date: {item[1]}</p>
+            <p>End Date: {item[2]}</p>
+            <p>Description: {item[3]}</p>
+            <p>Location: {item[4]}</p>
+            <button onClick={() => addEvent(item[0], item[1], item[2], item[3], item[4], index)} 
+            disabled={clicked[index]}>
+              {clicked[index] ? "Event Added" : "Add Event"}
+            </button>
+          </div>
         </>
       ))}
     </div>
